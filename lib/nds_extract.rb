554ee7dddaf982a845e0fbc0075dea9403a0266c
Cindy Cai <caicindy87@gmail.com>
2020-03-02 17:35:33 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,7 +10,39 @@ def directors_totals(nds)
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   
+  row_index = 0
+     name_array = []
+     while row_index < directors_database.length do
+      name_array.push(directors_database[row_index][:name])
+      row_index += 1
+     end
+     puts name_array
+ 
+    row_index = 0
+    totals_array = [] 
+    while row_index < directors_database.length do
+     gross_sum = 0
+     inner_array_index = 0
+     
+     while inner_array_index < directors_database[row_index][:movies].length do
+      gross_sum += directors_database[row_index][:movies][inner_array_index][:worldwide_gross]
+      inner_array_index += 1
+     end
+     row_index += 1
+     totals_array.push(gross_sum)
+     
+    end
+ 
 
+
+
+results_hash = {}
+directors_database.length.times { |index|
+  results_hash[name_array[index]] = totals_array[index]
+  
+  }
+
+puts results_hash
     
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
